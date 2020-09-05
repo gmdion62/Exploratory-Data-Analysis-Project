6958e0419fb6d371c8cdad94335055e144399e8e
gmdion62 <68613644+gmdion62@users.noreply.github.com>
2020-09-04 22:28:23 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -4,7 +4,7 @@ plot(household_power_consumption$DateTime,household_power_consumption$Voltage,ty
 plot(household_power_consumption$DateTime,household_power_consumption$Sub_metering_1,type = "l", lty = 1, ylab="Energy sub metering",xlab="")
 lines(household_power_consumption$DateTime,household_power_consumption$Sub_metering_2,col="red")
 lines(household_power_consumption$DateTime,household_power_consumption$Sub_metering_3,col="blue")
-legend("topright", lty=1, , lwd = 1, pch = "", col = c("black", "red","blue"), legend = c("Sub metering 1", "Sub metering 2","Sub metering 3" ), cex=1)
+legend("topright", c("Sub metering 1", "Sub metering 2","Sub metering 3" ), lty=1, , lwd = 1, pch = "", col = c("black", "red","blue"),  cex=1)
 plot(household_power_consumption$DateTime,household_power_consumption$Global_reactive_power,type = "l", lty = 1, ylab="Global_reactive_power",xlab="datetime")
 dev.copy(png, file = "plot4.png",width=480,height=480) ## Copy my plot to a PNG file
 dev.off() ## Don't forget to close the PNG device!
